096be7650c57df60b0cff7d7b1d968cfc0b0a53a
Rutuja Joshi <Rutuja Joshi@LAPTOP-7NPDJIQT>
2024-05-11 12:43:31 +0530

    Added method in about class

--- DmartExpressFunctionalAutomation/src/main/java/pom/AboutUs_Page.java
+++ DmartExpressFunctionalAutomation/src/main/java/pom/AboutUs_Page.java
@@ -47,5 +47,10 @@ public class AboutUs_Page {
 		return hours.getText();   // here i want to  use the code of html table => findelements , List collection 
     }
 	
+   public String getDaysAndHourss() {   // delete this method later
+		
+		return hours.getText();   // here i want to  use the code of html table => findelements , List collection 
+    }
+	
 
 }
